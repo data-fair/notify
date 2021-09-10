3cca6fe25cbea2c851fec3eb4f38d9ed04040a4c
Alban Mouton <alban.mouton@gmail.com>
2021-09-10 08:34:50 +0200

    chore: remove deprecated config vars

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,10 +1,8 @@
 module.exports = {
   port: 'PORT',
   publicUrl: 'PUBLIC_URL',
-  wsPublicUrl: 'WS_PUBLIC_URL',
   directoryUrl: 'DIRECTORY_URL',
   privateDirectoryUrl: 'PRIVATE_DIRECTORY_URL',
-  sessionDomain: 'SESSION_DOMAIN',
   mongoUrl: 'MONGO_URL',
   secretKeys: {
     identities: 'SECRET_IDENTITIES',
