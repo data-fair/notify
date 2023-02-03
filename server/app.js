6ddc0b3c4017355f37b923f6207b497c5a29034f
Alban Mouton <alban.mouton@gmail.com>
2023-02-03 17:28:58 +0100

    chore: longer incoming socket timeout

--- server/app.js
+++ server/app.js
@@ -24,6 +24,12 @@ app.set('json spaces', 2)
 app.set('session', session)
 const server = http.createServer(app)
 
+// cf https://connectreport.com/blog/tuning-http-keep-alive-in-node-js/
+// timeout is often 60s on the reverse proxy, better to a have a longer one here
+// so that interruption is managed downstream instead of here
+server.keepAliveTimeout = (60 * 1000) + 1000
+server.headersTimeout = (60 * 1000) + 2000
+
 if (process.env.NODE_ENV === 'development') {
   const proxy = require('http-proxy-middleware')
   // Create a mono-domain environment with other services in dev
