3b51bb31af3f2d99dd5633e19ad9e38a2caed09e
Alban Mouton <alban.mouton@gmail.com>
2024-09-24 09:20:42 +0200

    fix: error in upgrade script

--- upgrade/scripts/3.4.0/subscription-origin.js
+++ upgrade/scripts/3.4.0/subscription-origin.js
@@ -6,6 +6,6 @@ exports.exec = async (db, debug) => {
   const defaultOrigin = new URL(config.publicUrl).origin
   for await (const subscription of db.collection('subscriptions').find({ origin: { $exists: false } })) {
     const origin = subscription.urlTemplate ? new URL(subscription.urlTemplate).origin : defaultOrigin
-    await db.collection.updateOne({ _id: subscription._id }, { $set: { origin } })
+    await db.collection('subscriptions').updateOne({ _id: subscription._id }, { $set: { origin } })
   }
 }
